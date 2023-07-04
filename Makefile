634ee6b1af8a606c779e8749cd719c206fafa3ec
Gabriel Mendoza <gabrieldavid_98@hotmail.com>
2023-07-03 22:07:55 -0500

    Basic implementation

---
+++ Makefile
@@ -0,0 +1,11 @@
+##
+# Prefix Tree
+#
+# @file
+# @version 0.1
+
+default:
+	cc -Wall -Werror -std=c11 -pedantic -ggdb -o trie trie.c
+	./trie.exe
+
+# end
---
+++ trie.c
@@ -0,0 +1,57 @@
+#include <assert.h>
+#include <stdio.h>
+
+#define ARRAY_LEN(xs) (sizeof(xs) / sizeof((xs)[0]))
+
+typedef struct Node Node;
+
+struct Node {
+  Node *children[256];
+};
+
+#define NODE_POOL_CAP 1024
+Node node_pool[NODE_POOL_CAP];
+size_t node_pool_count = 0;
+
+Node *alloc_node(void) {
+  assert(node_pool_count < NODE_POOL_CAP);
+  return &node_pool[node_pool_count++];
+}
+
+void insert_text(Node *root, const char *text) {
+  if (text == NULL || *text == '\0') {
+    return;
+  }
+
+  assert(root != NULL);
+
+  size_t index = (size_t)*text;
+
+  if (root->children[index] == NULL) {
+    root->children[index] = alloc_node();
+  }
+
+  insert_text(root->children[index], text + 1);
+}
+
+void dump_dot(Node *root) {
+  size_t index = root - node_pool;
+  printf("    Node_%zu\n", index);
+  for (size_t i = 0; i < ARRAY_LEN(root->children); ++i) {
+    if (root->children[i] != NULL) {
+      size_t child_index = root->children[i] - node_pool;
+      printf("    Node_%zu -> Node_%zu\n", index, child_index);
+      dump_dot(root->children[i]);
+    }
+  }
+}
+
+int main(void) {
+  Node *root = alloc_node();
+  insert_text(root, "hello");
+  insert_text(root, "helium");
+  printf("digraph Trie {\n");
+  dump_dot(root);
+  printf("}\n");
+  return 0;
+}
